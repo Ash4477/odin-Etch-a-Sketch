b4487772eeb92c26b1f44c7ed5f3bbe0a68b6ba6
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2024-05-04 09:54:14 +0500

    give grid size button functionality

--- script.js
+++ script.js
@@ -11,4 +11,31 @@ function createGrid(gridSize) {
     }
 }
 
-createGrid(2);
+function removeGrid() {
+    const container = document.querySelector(".container");
+    container.innerHTML = "";
+}
+
+let gridSize = 16;
+
+createGrid(gridSize);
+
+const colorButton = document.querySelector("#color");
+const resetButton = document.querySelector("#reset");
+const sizeButton = document.querySelector("#size");
+
+sizeButton.addEventListener("click", () => {
+    while (true){
+        gridSize = (Number) (prompt("Enter Grid Size (1-100)"));
+        if (gridSize < 1 || gridSize > 100) {
+            alert("Invalid Size");
+        }
+        else {
+            removeGrid();
+            createGrid(gridSize);
+            return;            
+        }
+    }
+
+});
+
